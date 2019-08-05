c5b3720c93d53f759620bfcee72c49957569c8ed
Raphael Muenster <raphael.muenster@gmail.com>
2019-08-05 08:58:43 +0200

    Updated the app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { OurServicesComponent } from './our-services/our-services.component';
+import { OurNewsComponent } from './our-news/our-news.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    OurServicesComponent,
+    OurNewsComponent
   ],
   imports: [
     BrowserModule,
